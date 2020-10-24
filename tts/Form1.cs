db6788bbf341fbcf045bf75ff498b63608cd1097
Hector Nussbaumer <54723548+hectornuss@users.noreply.github.com>
2020-10-24 04:04:30 +0200

    Test

---
+++ tts/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace tts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SpeechSynthesizer ss_ = new SpeechSynthesizer();
+            ss_.SetOutputToDefaultAudioDevice();
+            ss_.Speak(richTextBox1.Text);
+        }
+    }
+}
